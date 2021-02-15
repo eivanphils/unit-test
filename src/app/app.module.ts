777c806511713ce4b02340fbd483d6f5e1e1290c
Phils <phils.garcia@multicaja.cl>
2021-02-15 15:29:45 -0300

    agregado unit testa hospitalComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
+import { HospitalComponent } from './intermedio2/hospital/hospital.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicosComponent,
-    MedicoComponent
+    MedicoComponent,
+    HospitalComponent
   ],
   imports: [
     BrowserModule,
