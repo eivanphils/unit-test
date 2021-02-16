5395d2b58d030aa5f304d010dba33c164bc92f64
Phils <phils.garcia@multicaja.cl>
2021-02-16 14:05:35 -0300

    agregado componente de rutas medico

--- src/app/avanzado/router-medico/router-medico.component.spec.ts
+++ src/app/avanzado/router-medico/router-medico.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, EMPTY, Subject, from } from 'rxjs';
 
 import { RouterMedicoComponent } from './router-medico.component';
+
+class FakeRouter{
+  navigate(params) {}
+}
 
 describe('RouterMedicoComponent', () => {
   let component: RouterMedicoComponent;
@@ -8,7 +14,16 @@ describe('RouterMedicoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RouterMedicoComponent ]
+      declarations: [ RouterMedicoComponent ],
+      providers: [
+        {provide: Router, useClass: FakeRouter},
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: from([{id: 'nuevo'}])
+          }
+        }
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +37,19 @@ describe('RouterMedicoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Debe de redireccionar a medico con los parametros', () => {
+    const router = TestBed.inject(Router);
+    const spy = spyOn(router, 'navigate');
+
+    component.guardarMedico();
+
+    expect(spy).toHaveBeenCalledWith(['medico', '123']);
+  });
+
+  it('Debe de colocar id = nuevo', () => {
+
+    expect(component.id).toBe('nuevo');
+  });
+
 });
--- src/app/avanzado/router-medico/router-medico.component.ts
+++ src/app/avanzado/router-medico/router-medico.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-router-medico',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./router-medico.component.sass']
 })
 export class RouterMedicoComponent implements OnInit {
+  id: string;
+  medico: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .subscribe( params => {
+      this.id = params['id']; // nuevo
+    });
+  }
+
+  guardarMedico() {
+    this.router.navigate(['medico', '123']);
   }
 
 }
