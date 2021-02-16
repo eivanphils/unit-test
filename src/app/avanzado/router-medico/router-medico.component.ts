74be6bf794042a2d1a8bf8f943d5804c2d200502
Phils <phils.garcia@multicaja.cl>
2021-02-16 13:08:51 -0300

    agregado componente de rutas medico

---
+++ src/app/avanzado/router-medico/router-medico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-router-medico',
+  templateUrl: './router-medico.component.html',
+  styleUrls: ['./router-medico.component.sass']
+})
+export class RouterMedicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
